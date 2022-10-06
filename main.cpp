1fd1c0b941a1a67eeaf0610d446ea9a9d07b64ad
Danilo Namitala <danilonamitala@yahoo.com.br>
2022-10-06 20:35:12 -0300

    asdsad

--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!! commit 2 sad\n";
-  std::cout << "Ola Troia\n";
-  std::cout << "Terceiro Print vai dar conflito!!\n";
-  return 0;
+  std::cout << "Hello World!!\n";
 }
