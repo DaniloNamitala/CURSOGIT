c219274f0e00b2848da38fa93518df51914dcbf7
Danilo Namitala <danilonamitala@yahoo.com.br>
2022-10-06 20:22:59 -0300

    Revert "commit 1"
    This reverts commit b926b3dfbd614d89c9f3822b83d6079d0ef785d7.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int main() {
   std::cout << "Hello World!! commit 2 sad\n";
-  std::cout << "Ola Troia commit 1!!\n";
+  std::cout << "Ola Troia\n";
   std::cout << "Terceiro Print vai dar conflito!!\n";
   return 0;
 }
