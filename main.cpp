c396569d91ab1f2881423473f1eba255fc57c375
Danilo Namitala <danilonamitala@yahoo.com.br>
2022-10-06 20:09:44 -0300

    conflito 2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main() {
   std::cout << "Hello World!!\n";
   std::cout << "Ola Troia!!\n";
-  std::cout << "Terceiro Print!!\n";
+  std::cout << "Terceiro Print nao vai dar conflito!!\n";
   return 0;
 }
